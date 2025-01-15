b365780589f6be4d74eefe32b8d8ece622b560a1
Raghav Shah <raghavshah900@gmail.com>
2025-01-15 11:22:30 -0600

    Commiting Portfolio gh changes

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -14,7 +14,7 @@ const Prices = () => {
       desc: "Innovative Use of GenAI in Agile Project Management: Utilized GenAI tools like Autogen, LangChain, and LangGraph to generate 20+ outputs, enhancing planning efficiency by 30%. Applied these tools to optimize Scrum-based sprints and estimations, ensuring precise execution across project phases.",
       tech: "Python, Langchain, Autogen, Langgraph, React ",
       deploy: "https://urban-ladder.vercel.app/",
-      github: "https://github.com/pidugu45/Urban-Ladder-Clone",
+      github: "https://github.com/RaghavShah01/GenAI-for-Software-Project-Management",
     },
     {
       id: 2,
@@ -23,7 +23,7 @@ const Prices = () => {
       desc: "• Integrated LIME and SHAP techniques, highlighting top 5 features influencing individual predictions, and increased model transparency and trustworthiness, achieving 98% accuracy on Hotel Review Management Model • Authored and published technical paper on project in Springer LNNS, volume 796, Jan 2024, demonstrating model effectiveness and interpretability techniques",
       tech: "Python, Lime, SHAP, XAI",
       deploy: "https://westside-ecommerce-clone.vercel.app/",
-      github: "https://github.com/mdalishanali/Westside-ecommerce-Clone",
+      github: "https://github.com/",
     },
     {
       id: 3,
@@ -31,7 +31,7 @@ const Prices = () => {
       heading: "Chat Application",
       desc: "• Developed a Flask-based application includes functionality joining 10+ chat groups and chatting • Built front-ends with HTML/CSS, Bootstrap, and JavaScript, and a Python, Flask, and Socket backend for real-time communication. Integrated Firebase for authentication and tested with 100+ users ",
       tech: "Python, Flask, Socket, HTML/CSS, Bootstrap,JavaScript.",
-      github: "https://github.com/mdalishanali/croma1",
+      github: "https://github.com/RaghavShah01/Chat-Application",
     },
     {
       id: 4,
@@ -39,7 +39,7 @@ const Prices = () => {
       heading: "BlogHost",
       desc: "It is Dynamic Blogging Website• On this Platform User can create there Account Post thereBlogs , and show their talent globally• User can select a listed Blogs and they can Read it & Like it.",
       tech: "HTML , CSS , Javascript , Node.js and MongoDB",
-      github: "https://github.com/mdalishanali/YoutubeCloneSearch",
+      github: "https://github.com/RaghavShah01/Blog-Host",
     },
   ]);
   return (
