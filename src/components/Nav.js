b288725f9a47fc8aa7ff8de0463516d0b7b09c85
Raghav Shah <raghavshah900@gmail.com>
2025-01-20 23:39:54 -0600

    fixing responsiveness

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,59 +1,57 @@
 import React from "react";
 import { Link } from "react-scroll";
-
 import { FaAlignJustify } from "react-icons/fa";
+
 const Nav = () => {
-  const [state, setState] = React.useState(false);
+  const [state, setState] = React.useState(false); // Set default state to `false` (menu is minimized by default)
+
   return (
     <nav className="navbar">
       <div className="container">
         <div className="navbar__container">
           <ul className="navbar__left">
             <div className="navbar__left-logo">
-              <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="logo" style={{ width: '250px' }}/>
+              <img src="/images/logo.png" alt="logo" style={{ width: "250px" }} />
             </div>
           </ul>
-          {state ? (
-            <ul className="navbar__right">
-              <Link spy={true} smooth={true} duration={500} to={"home"}>
-                <li>
-                  <a href="">Home</a>
-                </li>
-              </Link>
-
-              <Link spy={true} smooth={true} duration={500} to={"about"}>
-                <li>
-                  <a href="">About</a>
-                </li>
-              </Link>
-              <Link spy={true} smooth={true} duration={500} to={"skills"}>
-                <li>
-                  <a href="">Skills</a>
-                </li>
-              </Link>
-              <Link spy={true} smooth={true} duration={500} to={"project"}>
-                <li>
-                  <a href="">Project</a>
-                </li>
-              </Link>
-              <Link spy={true} smooth={true} duration={500} to={"contact"}>
-                <li>
-                  <a href="">Contact</a>
-                </li>
-              </Link>
+          <ul className={`navbar__right ${state ? "active" : ""}`}>
+            <Link spy={true} smooth={true} duration={500} to={"home"}>
               <li>
-                <a
-                  href="https://drive.google.com/file/d/1rU7_ISF-QTm9z5uPRabp6ebQVc-umgmt/view?usp=sharing"
-                  target="_blank"
-                  className="btn btn-outline"
-                >
-                  Resume
-                </a>
+                <a href="">Home</a>
               </li>
-            </ul>
-          ) : (
-            ""
-          )}
+            </Link>
+
+            <Link spy={true} smooth={true} duration={500} to={"about"}>
+              <li>
+                <a href="">About</a>
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} duration={500} to={"skills"}>
+              <li>
+                <a href="">Skills</a>
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} duration={500} to={"project"}>
+              <li>
+                <a href="">Project</a>
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} duration={500} to={"contact"}>
+              <li>
+                <a href="">Contact</a>
+              </li>
+            </Link>
+            <li>
+              <a
+                href="https://drive.google.com/file/d/1rU7_ISF-QTm9z5uPRabp6ebQVc-umgmt/view?usp=sharing"
+                target="_blank"
+                className="btn btn-outline"
+                rel="noopener noreferrer"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="toggle" onClick={() => setState(!state)}>
